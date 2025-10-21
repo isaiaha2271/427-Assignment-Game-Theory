983b701d7487a2aaa0fd0feb6e2afa2fd30f4a85
ojoro <zaiya4002@gmail.com>
2025-10-21 15:46:36 -0700

    Fixed bug with all_possible_paths bug, function calls

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -1,86 +1,85 @@
 import argparse
-import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from itertools import islice
 from utils import helper
-import os
+
+
 
 
 # MAIN FUNCTION
 def main():
-  # PARSER SECTION
-  # Creates the parser object in order to read in the passed in arguments and flags.
-  #   Utilizes the argparse library to ease the implementation for a working CL structure.
-  parser = argparse.ArgumentParser(
-    prog="Social Networks and Structural Balance",
-    description="This program will help generate randomized graphs and analyze them."
-  )
+    # PARSER SECTION
+    # Creates the parser object in order to read in the passed in arguments and flags.
+    #   Utilizes the argparse library to ease the implementation for a working CL structure.
+    parser = argparse.ArgumentParser(
+        prog="Social Networks and Structural Balance",
+        description="This program will help generate randomized graphs and analyze them."
+    )
 
-  # Adds additional options and arguments to the parser, accoring to this CL structure:
-  #   python ./graph.py ..
-  parser.add_argument("input", type=str)
-  parser.add_argument("n", type=int)
-  parser.add_argument("initial", type=int)
-  parser.add_argument("final", type=int)
-  parser.add_argument("--plot", action="store_true")
-
-  
-
-  # Parses and gathers the arguments
-  args = parser.parse_args()
-    
-  # Handles if there are not sufficient parameters
-  if not args.plot:
-    print("--plot is not present. The graph will not be shown.")
-  print()
-    
-  # GRAPH CONSTRUCTION SECTION
-  graph = None
+    # Adds additional options and arguments to the parser, accoring to this CL structure:
+    #   python ./graph.py ..
+    parser.add_argument("input", type=str)
+    parser.add_argument("--n", type=int)
+    parser.add_argument("--initial", type=int)
+    parser.add_argument("--final", type=int)
+    parser.add_argument("--plot", action="store_true")
 
     
-  if (args.input):
-    try:
-      graph = nx.read_gml(f"data/{args.input}")
-    except nx.NetworkXError as e:
-      print(f"Error: Could not read the file as a GML graph: {e}")
-      print("Please ensure the file is a valid GML format.")
-      return
-    except FileNotFoundError:
-      print(f"File `data/{args.input}` was not found. Please specify an existing .gml file inside the `data/` directory.")
-      return
-  else:
-    print("No --input No graph has been loaded.")
 
-    
-  
-    intial = None
+    # Parses and gathers the arguments
+    args = parser.parse_args()
+        
+    # Handles if there are not sufficient parameters
+    if not args.plot:
+        print("--plot is not present. The graph will not be shown.")
+    print()
+        
+    # GRAPH CONSTRUCTION SECTION
+    graph = None
+
+        
+    if (args.input):
+        try:
+            graph = nx.read_gml(f"data/{args.input}")
+            
+        except nx.NetworkXError as e:
+            print(f"Error: Could not read the file as a GML graph: {e}")
+            print("Please ensure the file is a valid GML format.")
+            return
+        except FileNotFoundError:
+            print(f"File `data/{args.input}` was not found. Please specify an existing .gml file inside the `data/` directory.")
+            return
+    else:
+        print("No --input No graph has been loaded.")
+
+        
+    initial = None
     final = None
     n = None
-  
+    all_paths = None
+
     
-    if args.initial and args.final and args.n:
-      intial = args.initial
-      final = args.final
-      n = args.n
-      
-      social_optima = helper.compute_social_optima(graph, intial,final,n)
-      travel_eq = helper.compute_travel_eq(graph,intial,final,n)
-      print(f"Social Optima: {social_optima}")
-      print(f"Travel Equilibrium: {travel_eq}")
+    if args.initial is not None and args.final is not None and args.n is not None:
+        initial = args.initial
+        final = args.final
+        n = args.n
+        all_paths = helper.all_possible_paths(graph, str(initial),str(final)) # get all possbile paths from start node to end node 
+        social_optima = helper.compute_social_optima(graph, all_paths,n)
+        travel_eq = helper.compute_travel_eq(graph,all_paths,n)
+        print(f"Social Optima: {social_optima}")
+        print(f"Travel Equilibrium: {travel_eq}")
 
 
-  
-  
-  
-  
-  # GRAPH PLOTTING SECTION
-  if args.plot:
-    helper.plot_results(graph)
-    pass
-    
-  else:
-    print("There was no graph to be displayed...")
+
+
+
+
+    # GRAPH PLOTTING SECTION
+    if args.plot:
+        helper.plot_results(graph)
+        pass
+    else:
+        print("There was no graph to be displayed...")
 
 
 
--- utils/helper.py
+++ utils/helper.py
@@ -8,18 +8,13 @@ def intialize_flows(G):
     flows = {(u,v): 0 for u,v in G.edges()}
     return flows
 
-def compute_social_optima(G, intial,final,n):
-    pass
-
-
-
-def compute_travel_eq(G,paths, n):
+def compute_social_optima(G, paths,n):
     num_paths = len(paths)
     vehicles_per_path = n/num_paths
     flow_dict = intialize_flows(G)
 
 
-
+    #update flow dict to evenly distribute vehciles for each
     for path in paths:
         for i in range(len(path)-1):
             u = path[i]
@@ -30,10 +25,83 @@ def compute_travel_eq(G,paths, n):
     iteration_limit = 100
     iter1 = 0
 
-
+    #adjust network flows iteratively
     while iter1 < iteration_limit:
+              
+
+        marg_cost_dict = {}
+        #compute marginal cost for a given path in the graph
+        for u,v in flow_dict:
+            a = G[u][v]['a']
+            marg_cost_dict[(u,v)] = compute_travel_time(G,u,v,flow_dict[(u,v)]) +flow_dict[(u,v)]*a
+        
+
+        path_costs = {}
+
+        #compute travel time for each path and keep track of path with smallest cost
+        min_path = None
+        for path in paths:  
+            cost = sum(marg_cost_dict[(path[i],path[i+1])] for i in range(len(path)-1))
+            path_costs[tuple(path)] = cost
+            if not min_path:
+                min_path= (path,cost)
+            else:
+                if cost < min_path[1]:
+                    min_path = (path,cost)
+
+        shortest_path,min_cost = min_path
+
+        #check if all nonzero paths are roguhly equal
+        converged = True 
+        for path in path_costs:
+            if sum(flow_dict[(path[i],path[i+1])] for i in range(len(path)-1))> 0: #checking paths with flow >0 for equilibirum 
+                if abs(path_costs[tuple(path)]-min_cost) > convergence_threshold:
+                    converged = False
+                    break
+        if converged: #all paths are within 0.001 cost difference(they are roughly same), thus social optimum reach 
+            break
+                
+
+        #shift flow towards shortest path
+        step_size = 0.1
+        direction_flow = {edge:0 for edge in flow_dict}
+        flow_per_path = n*step_size
+        for i in range(len(shortest_path)-1):
+            edge = (shortest_path[i], shortest_path[i+1])
+            direction_flow[edge] += flow_per_path
 
 
+        
+
+        # Update flows for each edge using weighted average
+        for edge in flow_dict:
+            flow_dict[edge] += (1-step_size)*flow_dict[edge]+step_size*direction_flow[edge]
+        
+        i+=1
+    return flow_dict
+
+
+
+#compute travel equilibrium for a given graph, paths, and number of cars
+def compute_travel_eq(G,paths, n):
+    num_paths = len(paths)
+    vehicles_per_path = n/num_paths
+    flow_dict = intialize_flows(G)
+
+
+    #update flow dict to evenly distribute vehciles for each
+    for path in paths:
+        for i in range(len(path)-1):
+            u = path[i]
+            v = path[i+1]
+            flow_dict[(u,v)] += vehicles_per_path
+        
+    convergence_threshold = 0.001
+    iteration_limit = 100
+    iter1 = 0
+
+    #adjust network flows iteratively
+    while iter1 < iteration_limit:
         path_costs = {}
 
         #compute travel time for each path and keep track of path with smallest cost
@@ -61,19 +129,22 @@ def compute_travel_eq(G,paths, n):
             break
                 
 
+
         #shift flow towards shortest path
+        step_size = 0.1
         direction_flow = {edge:0 for edge in flow_dict}
-        flow_per_path = n
+        flow_per_path = n*step_size
+
         for i in range(len(shortest_path)-1):
             edge = (shortest_path[i], shortest_path[i+1])
             direction_flow[edge] += flow_per_path
 
 
-        step_size = 0.5
+       
 
         # Update flows for each edge using weighted average
         for edge in flow_dict:
-            flow_dict = (1-step_size)*flow_dict[edge]+step_size*direction_flow[edge]
+            flow_dict[edge] += (1-step_size)*flow_dict[edge]+step_size*direction_flow[edge]
 
 
 
